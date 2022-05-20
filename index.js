7efeaaca9a1b4ef47933b01d7499ba7164b1b78b
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-20 13:26:31 -0400

    commit tree d6b5fd3b5dcbd8a4f880f9576ab162182bd3fa01

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(names, string){
+    let filteredArray = names.filter(name => name.toLowerCase() === string.toLowerCase())
+    return filteredArray
+    }
+    
+function fuzzyMatch(names, string){
+    let filteredArray = names.filter(name => name.substring(0, string.length) === string)
+        return filteredArray
+    }
+
+
+function matchName(arr, string){
+    return arr.filter(driver => driver.name === string);
+}
